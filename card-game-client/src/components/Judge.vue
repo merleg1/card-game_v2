f1854e064f9c9cc7081ae6c53999f5af4a3a620f
merleg1 <aaron.lengen@stud.hslu.ch>
2023-04-18 14:23:46 +0200

    packages removed

--- card-game-client/src/components/Judge.vue
+++ card-game-client/src/components/Judge.vue
@@ -1,49 +1,44 @@
 <template>
-    <carousel3d class="judge" :width="200" :height="400" :inverse-scaling="50" :space="50" :perspective="15">
-      <slide  :index="i" class="playing-card" v-for="card in sData.cardsToJudge" :key="card.setId + '.' + card.id"
-        :id="card.setId + '.' + card.id">
-        <div class="playing-card-face">
-          <div class="playing-card-label">
-            {{ card.text }}
-          </div>
+    <div class="card-stack">
+        <div class="stack-card" v-for="card in sData.cardsToJudge" :key="card.id">
+            <div class="stack-card-face">
+                <div class="stack-card-label">{{ card.text }}</div>
+            </div>
         </div>
-      </slide>
-    </carousel3d>
+    </div>
 </template>
 
 <script>
 import { socketData, socket } from '../socket';
 import { useQuasar } from 'quasar';
-import { carousel3d, slide } from 'vue-carousel-3d';
 
 export default {
-name: 'Judge',
-components: {
-    'carousel3d': window['carousel-3d'].Carousel3d,
-    'slide': window['carousel-3d'].Slide
-},
-data: function () {
+  name: 'Judge',
+  components: {
+
+  },
+  data: function () {
     return {
-    sData: socketData,
-    shareUrl: window.location.origin + "/join-room/" + this.$route.params.roomCode,
-    cardsPicked: [],
-    $q: useQuasar(),
+      sData: socketData,
+      $q: useQuasar(),
     }
-},
+  },
 }
 </script>
 
 
 <style>
-body {
+/* body {
   max-width: 375px;
   margin: 10px auto;
-}
+} */
 
 .carousel-3d-container .carousel-3d-slide {
-    padding: 20px;
+  padding: 20px;
 }
 
-.carousel-3d-container .carousel-3d-slide .title { font-size: 22px; }
+.carousel-3d-container .carousel-3d-slide .title {
+  font-size: 22px;
+}
 </style>
 
